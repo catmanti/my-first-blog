6ab770a6e8aa837cf1091b25161e280d67fa59f0
catman <somaratne@yahoo.com>
2019-04-19 10:51:39 +0530

    Modified templates to diploy posts from database.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,12 @@ It will request information from the model you created before
 and pass it to a template.
 '''
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	context = {
+		'my_name' : 'Catman Tiger.',
+		'my_address' : 'Peradeniya'
+		}
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts})
