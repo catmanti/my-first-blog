698f9e970dbc6c456d3723602b5806e1576b0e81
catman <somaratne@yahoo.com>
2019-04-19 21:30:38 +0530

    added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ A view is a place where we put the "logic" of our application.
 It will request information from the model you created before 
 and pass it to a template.
 '''
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 def post_list(request):
@@ -13,3 +13,7 @@ def post_list(request):
 		}
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html',{'post': post})
