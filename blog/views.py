ff3a61067a16e1f7cae68885556c259eda46141b
catman <somaratne@yahoo.com>
2019-04-20 05:35:45 +0530

    Test forms.py

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,11 @@ A view is a place where we put the "logic" of our application.
 It will request information from the model you created before 
 and pass it to a template.
 '''
+<<<<<<< Updated upstream
 from django.shortcuts import render,get_object_or_404
+=======
+from django.shortcuts import render
+>>>>>>> Stashed changes
 from django.utils import timezone
 from .models import Post
 def post_list(request):
@@ -12,8 +16,13 @@ def post_list(request):
 		'my_address' : 'Peradeniya'
 		}
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+<<<<<<< Updated upstream
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html',{'post': post})
+=======
+	print(posts[1])
+	return render(request, 'blog/post_list.html', {'posts':posts})
+>>>>>>> Stashed changes
