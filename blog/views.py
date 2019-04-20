79165b16033d6cc0c37eaa1449bd56862b445a40
catman <somaratne@yahoo.com>
2019-04-20 19:19:22 +0530

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -3,26 +3,48 @@ A view is a place where we put the "logic" of our application.
 It will request information from the model you created before 
 and pass it to a template.
 '''
-<<<<<<< Updated upstream
-from django.shortcuts import render,get_object_or_404
-=======
-from django.shortcuts import render
->>>>>>> Stashed changes
+from django.shortcuts import render,get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+
 def post_list(request):
 	context = {
 		'my_name' : 'Catman Tiger.',
 		'my_address' : 'Peradeniya'
 		}
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-<<<<<<< Updated upstream
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html',{'post': post})
-=======
-	print(posts[1])
-	return render(request, 'blog/post_list.html', {'posts':posts})
->>>>>>> Stashed changes
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()		
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)		
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+
